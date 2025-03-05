5777275d5171ba1192251ef947a648ddc2f93d56
Russell Gordon <rgordon@lcs.on.ca>
2025-03-04 21:47:30 -0500

    Initial Commit

---
+++ TriviaMVVM/TriviaMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaMVVMApp.swift
+//  TriviaMVVM
+//
+//  Created by Russell Gordon on 2025-03-04.
+//
+
+import SwiftUI
+
+@main
+struct TriviaMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
